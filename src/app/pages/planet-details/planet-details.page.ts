faad939d036b94157982bb1b56b645bb9c3b5131
BrenKenna <brend.k.bk@gmail.com>
2022-04-22 11:28:18 +0000

    planet details commit

--- src/app/pages/planet-details/planet-details.page.ts
+++ src/app/pages/planet-details/planet-details.page.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+// Support loading page with input from another page
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-planet-details',
   templateUrl: './planet-details.page.html',
@@ -7,9 +11,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlanetDetailsPage implements OnInit {
 
-  constructor() { }
-
+  planet: any;
+ 
+  constructor(private activatedRoute: ActivatedRoute, private http: HttpClient) { }
+ 
   ngOnInit() {
+    let id = this.activatedRoute.snapshot.paramMap.get('id');
+    this.http.get(`https://swapi.dev/api/planets/${id}`).subscribe(res => {
+      this.planet = res;
+    });
   }
 
 }
--- src/app/pages/planets/planets.page.ts
+++ src/app/pages/planets/planets.page.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+// Modules for consuming API and routing to another page with results
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-planets',
   templateUrl: './planets.page.html',
@@ -7,9 +12,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlanetsPage implements OnInit {
 
-  constructor() { }
-
+  planets: Observable<any>;
+ 
+  constructor(private router: Router, private http: HttpClient) { }
+ 
   ngOnInit() {
+    this.planets = this.http.get('https://swapi.dev/api/planets');
+    this.planets.subscribe(data => {
+      console.log('my data: ', data);
+    });
+  }
+ 
+
+  openDetails(planet) {
+    let split = planet.url.split('/');
+    let planetId = split[split.length-2];
+    this.router.navigateByUrl(`/tabs/planets/${planetId}`);
   }
 
 }
